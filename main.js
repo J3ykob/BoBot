4245fe1f6582e5df2ceb0d0d81cf07c2015e120a
J3ykob <59123462+J3ykob@users.noreply.github.com>
2020-09-04 10:12:23 +0200

    URL search function
    finds specific URL with a help of keywords

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const https = require("https");
 
+var keywords = "Camo";
+var color = "Black";
+
+var CSRFTOKEN = "";
+
 const options = {
 	host: "151.101.114.133",
 	port: 443,
@@ -7,20 +12,38 @@ const options = {
 	method: "GET",
 	headers: {
 		accept:
-			"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+			"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
 		"accept-language": "pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7",
 		"cache-control": "no-cache",
 		host: "www.supremenewyork.com",
 	},
 };
 
+var searchByKeyWords = function (keyword, document) {
+	//finds specific URL with a help of keywords
+	let splitted = document.split("article");
+	var i = 0;
+	while (
+		splitted[i].search(keyword) == -1 ||
+		splitted[i].search(color) == -1 ||
+		i > splitted.lenth
+	) {
+		i++;
+	}
+	return splitted[i].split('href="', 2)[1].split('"')[0];
+};
+
 https.get(options, (res) => {
 	var response = "";
 	res.on("data", (d) => {
-		response = `${d}`
+		response += `${d}`;
+	});
+	res.on("end", () => {
+		CSRFTOKEN = response
 			.split('name="csrf-token" content="', 2)[1]
 			.split('"', 1)[0];
-		console.log(response);
-		res.removeAllListeners("data");
+		console.log(CSRFTOKEN);
+
+		console.log(searchByKeyWords(keywords, response));
 	});
 });
